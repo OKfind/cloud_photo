0a8aa52607b7c578d24567181dcf293f4a517dc0
OKfind <1649041715@qq.com>
2026-06-10 09:42:21 +0800

    fix: 修改Cloudflare部署的配置

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,7 +1,19 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
+import type { OpenNextConfig } from "@opennextjs/cloudflare";
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
-export default defineCloudflareConfig({
+const config = defineCloudflareConfig({
   incrementalCache: r2IncrementalCache,
-});
+}) satisfies OpenNextConfig;
+
+export default {
+  ...config,
+  middleware:
+    config.middleware?.external === true
+      ? {
+          ...config.middleware,
+          runtime: "edge",
+        }
+      : config.middleware,
+} satisfies OpenNextConfig;
